9b00b876d8b4a63f153c59fe5c84cf04440d9510
Lena Jeremiah <49532117+Jeremiahjacinth13@users.noreply.github.com>
2021-11-15 06:21:36 +0100

    completed bubble sory

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -41,8 +41,8 @@ bubbleSort([10, 5, 3, 8, 2, 6, 4, 7, 9, 1]);
 
 // unit tests
 // do not modify the below code
-// test("bubble sort", function () {
-//   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
-//   const sortedNums = bubbleSort(nums);
-//   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-// });
+test("bubble sort", function () {
+  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+  const sortedNums = bubbleSort(nums);
+  expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+});
